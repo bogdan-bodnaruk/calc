491e454a7346eca516f47e51e2c33290e0061d36
Bogdan-Bodnaruk-gaf <Bogdan.Bodnaruk@gaf.energy>
2025-04-28 11:51:46 +0300

    Move the project to vite Update a task list

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -4,17 +4,18 @@ export default function Tasks() {
       <h2>Task list:</h2>
       <ul>
         <li>
-          Find and fix bug on input. User should be able to add numbers only by
-          clicking buttons
+          Find and fix the bug on input. The user should be able to add numbers only by clicking buttons
         </li>
-        <li>Find and fix bug related "equal" and "clear" functionality</li>
+        <li>Find and fix bugs related to "equal" and "clear" functionality</li>
         <li>Improve typing, remove ts-ignore lines, make sure build command works without errors</li>
-        <li>Simplify Calculator component by using DRY and KISS</li>
+        <li>Simplify the Calculator component by using DRY and KISS</li>
         <li>
-          Write unit test to cover Calculator component. Context should not be
-          mocked.
+          Write a behavioural unit test to cover the Calculator component. Context should not be mocked.
         </li>
       </ul>
+      <p>
+        To complete the project, you can use any tools like searching on the internet, AI, code examples, etc...
+      </p>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,9 @@ export default defineConfig({
   },
   plugins: [
     react(),
-  ]
+  ],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
 });
